207e6c079e3f26c6d70b08d7c839d59d7e73f993
Leena Jain <leenakjain@gmail.com>
2017-04-27 20:36:50 +1000

    added Product Service

--- ordermanagement/src/main/java/com/lnk/marts/service/ProductService.java
+++ ordermanagement/src/main/java/com/lnk/marts/service/ProductService.java
@@ -1,5 +1,34 @@
 package com.lnk.marts.service;
 
+import com.lnk.marts.domain.Product;
+import com.lnk.marts.domain.ProductCategory;
+import com.lnk.marts.repo.ProductCategoryRepo;
+import com.lnk.marts.repo.ProductRepo;
+
 public class ProductService {
 
+	public ProductService(ProductRepo productRepo , ProductCategoryRepo productCategoryRepo) {
+		this.productRepo=productRepo;
+		this.productCategoryRepo=productCategoryRepo;
+	}
+
+	private ProductRepo productRepo;
+	private ProductCategoryRepo productCategoryRepo;
+	
+	
+	/*public Product createProduct()
+	{
+		
+		 ProductCategory productCategory = productCategoryRepo.findOne("a");
+		 
+	        if (product == null) {
+	            throw new RuntimeException("Product categpru does not exist: " + "aa");
+	        }
+	        return productRepo.save(product);
+	        
+	}*/
+	
+	
+
+	
 }
